e3454c021c31f7d161f36bb1738693a04d9d99e2
ggian <geogiadim@webmail.auth.gr>
2020-04-08 01:38:59 +0300

    search bar fixed, works properly

--- CleanEnergyProject/menu-footer/navBar.js
+++ CleanEnergyProject/menu-footer/navBar.js
@@ -2,7 +2,7 @@
 function myFunction() {
     var x = document.getElementById("search-label");
     if (x.style.display === "none") {
-        x.style.display = "block";
+        x.style.display = "flex";
     } else {
         x.style.display = "none";
     }
