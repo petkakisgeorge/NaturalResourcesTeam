9aca3416635d58ee42a82fd5ded206d7770ea6ee
george <petkakisgiwrgos@gmail.com>
2020-04-15 18:33:56 +0300

    United Site

--- CleanEnergyProject/menu-footer/navBar.js
+++ CleanEnergyProject/menu-footer/navBar.js
@@ -6,10 +6,23 @@ function myFunction() {
         x.style.display = "";
     }
 }
-function PageScroll() {
-
-    setTimeout(sco,500);
+/* Always when changing a page ,it is scrolling up - useful for visiting a page's section.
+* But because even when we refresh it keeps scrolling, we handle it so it doesn't scroll! */
+const Scroll= checkRefresh();
+function checkRefresh()
+{
+    //check for Navigation Timing API support
+    if (performance.navigation.type === 1) {
+        //This page is reloaded
+    } else {
+        //This page is not reloaded
+        setTimeout(sco,500);
+    }
 }
 function sco() {
     window.scrollBy(0, -100); // Scroll 100px up
+}
+/* This function is only for Technology Section*/
+function PageScroll() {
+    setTimeout(sco,500);
 }
